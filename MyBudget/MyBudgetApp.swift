4f32ba46379fb78cf9ac8d3f5faa72d268e55f93
Ahmed Hazzaa <ahazzaa@arabdt.com>
2024-08-26 12:09:54 +0300

    Initial Commit

---
+++ MyBudget/MyBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBudgetApp.swift
+//  MyBudget
+//
+//  Created by A.Hazzaa on 26/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
